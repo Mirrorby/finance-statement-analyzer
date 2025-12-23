8b4c6ebce6aa04461a601a0b4bc373da18f79153
Mirrorby <171128087+Mirrorby@users.noreply.github.com>
2025-12-23 12:09:26 +0300

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BankStatementAnalyzer from './app/BankStatementAnalyzer';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BankStatementAnalyzer />
+  </React.StrictMode>
+);
